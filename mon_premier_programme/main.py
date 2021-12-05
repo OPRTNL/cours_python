7b918537622bebefab95d419c87ea9a051600436
maxime berger <berger.pro.maxime@gmail.com>
2021-12-05 20:03:15 +0100

    stade try/except

---
+++ mon_premier_programme/main.py
@@ -0,0 +1,23 @@
+print ('vouvou')
+
+# demander les variable
+nom = input("ton nom connard :")
+age = input("ton age fils de pute")
+
+try:
+    age_int = int(age)
+except:
+    print("ERREUR : l'age n'est pas un nombre")
+else:
+    print("Tu t'appelle " + nom + " et ton age de vieille merde est " + str(age_int) + " ans.")
+    
+
+
+#test variable
+
+"""imprimer le bordel
+les blaireau
+"""
+
+
+
