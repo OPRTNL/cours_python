5e3e75022708cab454ca757aae8732317fb0957a
maxime berger <berger.pro.maxime@gmail.com>
2021-12-06 15:45:17 +0100

    les fonctions et return

--- mon_premier_programme/main.py
+++ mon_premier_programme/main.py
@@ -1,3 +1,14 @@
+def demander_age():
+    age_int = 0
+    while age_int == 0:
+        age_str = input("ton age ")
+        try:
+            age_int = int(age_str)
+        except:
+            print("ERREUR : l'age n'est pas un nombre")
+    return age_int
+
+
 #  demander la variable nom avec une vrai variable unique
 nom = input("ton nom  :")
 while nom == "":
@@ -5,15 +16,9 @@ while nom == "":
     nom = input("ton nom  :")
 
 #initialisation varaible
-age = 0
 
 #boucle pour demander l'age en testant la validité 
-while age == 0:
-    age_str = input("ton age ")
-    try:
-        age = int(age_str)
-    except:
-        print("ERREUR : l'age n'est pas un nombre")
+age = demander_age()
 
 print("Tu t'appelle " + nom + " et ton age est " + str(age) + " ans.")
 print("L'année prochaine tu aura " + str(age+1))
