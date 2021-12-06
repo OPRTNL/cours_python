cc013ad3ae1de0410321a44e982625837e8709c5
maxime berger <berger.pro.maxime@gmail.com>
2021-12-06 16:13:01 +0100

    variables globales et fonctions

--- mon_premier_programme/main.py
+++ mon_premier_programme/main.py
@@ -1,12 +1,13 @@
-def demander_age():
+def demander_age(nom_du_gars):
     age_int = 0
     while age_int == 0:
-        age_str = input("ton age ")
+        age_str = input(nom_du_gars + " quel est ton age ")
         try:
             age_int = int(age_str)
         except:
             print("ERREUR : l'age n'est pas un nombre")
     return age_int
+
 
 def demander_nom():
     nom_str = input("ton nom  :")
@@ -15,12 +16,14 @@ def demander_nom():
         nom_str = input("ton nom  :")
     return nom_str
 
+def show_text(nom_str, age_int):
+    print("Tu t'appelle " + nom_str + " et ton age est " + str(age_int) + " ans.")
+    print("L'année prochaine tu aura " + str(age_int+1))
 
 #  demander la variable nom avec une vrai variable unique
 nom = demander_nom()
 
 #boucle pour demander l'age en testant la validité 
-age = demander_age()
+age = demander_age(nom)
 
-print("Tu t'appelle " + nom + " et ton age est " + str(age) + " ans.")
-print("L'année prochaine tu aura " + str(age+1))
+show_text(nom, age)
