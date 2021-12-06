f091d08ea9df1cb68faff0b0e3528ab82b6cb189
maxime berger <berger.pro.maxime@gmail.com>
2021-12-06 15:01:34 +0100

    boucle while

--- mon_premier_programme/main.py
+++ mon_premier_programme/main.py
@@ -1,23 +1,15 @@
-print ('vouvou')
+#  demander les variable
+nom = input("ton nom  :")
+#initialisation varaible age
+age = 0
 
-# demander les variable
-nom = input("ton nom connard :")
-age = input("ton age fils de pute")
+#boucle pour demander l'age en testant la validité 
+while age == 0:
+    age_str = input("ton age ")
+    try:
+        age = int(age_str)
+    except:
+        print("ERREUR : l'age n'est pas un nombre")
 
-try:
-    age_int = int(age)
-except:
-    print("ERREUR : l'age n'est pas un nombre")
-else:
-    print("Tu t'appelle " + nom + " et ton age de vieille merde est " + str(age_int) + " ans.")
-    
-
-
-#test variable
-
-"""imprimer le bordel
-les blaireau
-"""
-
-
-
+print("Tu t'appelle " + nom + " et ton age est " + str(age) + " ans.")
+print("L'année prochaine tu aura " + str(age+1))
