66b26a3b0f3d2a06131dcb772a8ab7d74993b0c5
maxime berger <berger.pro.maxime@gmail.com>
2021-12-07 18:22:17 +0100

    test ok

--- mon_premier_programme/jeu_de_maths/main.py
+++ mon_premier_programme/jeu_de_maths/main.py
@@ -4,6 +4,7 @@ NOMBRE_MIN = 1
 NOMBRE_MAX = 10
 NB_QUESTIONS = 3
 
+
 def ask_question():
     a = random.randint(NOMBRE_MIN, NOMBRE_MAX)
     b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
@@ -20,12 +21,14 @@ def ask_question():
             answer_int = int(answer_str)
         except:
             print("ERREUR : La réponse n'est pas un nombre !")
+
     if answer_int == result:
         print("Bonne réponse")
         return True
     else: 
         print("Faux la réponse était", result)
         return False
+
 
 def questions():
     question = NB_QUESTIONS
@@ -39,6 +42,7 @@ def questions():
     print(f"Test terminé ! Votre score est de {score}/{NB_QUESTIONS}")
     return score
 
+
 def appreciation():
     mean = NB_QUESTIONS/2
     score = questions()
@@ -47,5 +51,6 @@ def appreciation():
         print("Excellent") if score == NB_QUESTIONS else print("Pas Mal!")
     else :
         print("Révisez vos maths") if score == 0 else print("Peut mieux faire")
+
 
 appreciation()
