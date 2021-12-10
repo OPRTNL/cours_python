5bb30f58ae8a64f491010f36b9a5bb6252fb03c3
maxime berger <berger.pro.maxime@gmail.com>
2021-12-10 16:35:04 +0100

    another way to do it

--- niveau_2/exercice_algorihtme/main.py
+++ niveau_2/exercice_algorihtme/main.py
@@ -3,13 +3,16 @@ distance_chauffeur_kilometre = [1.5, 2.2, 0.4,0.9,7.1,1.1,0.6]
 
 distance_min = distance_chauffeur_kilometre[0]
 
-for distance in distance_chauffeur_kilometre:
-    if distance < distance_min:
-        distance_min = distance
+i = 0
+index_ok = 0
 
-sorted_chauffeurs = distance_chauffeur_kilometre.sort()
-
-index = distance_chauffeur_kilometre.index(distance_min)
+while True:
+    if distance_min > distance_chauffeur_kilometre[i] :
+        distance_min = distance_chauffeur_kilometre[i]
+        index_ok = i
+    elif i == len(distance_chauffeur_kilometre) - 1:
+        break
+    i += 1
 
 print(f"distance minimale {distance_min} km")
-print(f"Votre chauffeur est {nom_chauffeurs[index]}")
+print(f"Votre chauffeur est {nom_chauffeurs[index_ok]}")
