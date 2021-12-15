679275a3189e3ceef93d514a5f79d8526213775e
maxime berger <berger.pro.maxime@gmail.com>
2021-12-15 17:54:16 +0100

    sum et join et len

--- niveau_2/expertise_collection/les-collections-avance-12/main.py
+++ niveau_2/expertise_collection/les-collections-avance-12/main.py
@@ -4,8 +4,21 @@
 #          0        1        2           3           4      5
 noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe", "Martin"]
 
-#
+"""
+# 1 for et len
 nb_caractere = 0
 for nom in noms: nb_caractere += len(nom) 
 
 print(nb_caractere)
+
+# 2 sum
+noms_len = sum([len(nom) for nom in noms])
+"""
+
+#3 join et len
+
+noms_len = len("".join(noms))
+
+
+print(noms_len)
+
