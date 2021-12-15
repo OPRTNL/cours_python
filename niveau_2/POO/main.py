6e84271df457aebcd994ed42d77a63a3122fd239
maxime berger <berger.pro.maxime@gmail.com>
2021-12-15 19:18:36 +0100

    Exercice demander le nom des personnes

--- niveau_2/POO/main.py
+++ niveau_2/POO/main.py
@@ -2,18 +2,28 @@
 ## DEFINITION 
 
 class Personne:
-    def __init__(self, nom):
+    def __init__(self, nom : str, age : int):
         self.nom = nom
+        self.age = age
+
+    def Donner_son_nom_et_son_age(self):
+        print(f"Bonjour je m'appelle {self.nom} et j'ai {self.age}" + (" ans" if not self.age == 1 else " an") + ".")
         
     def Se_presenter(self):
         print("Bonjour je m'appelle : " + self.nom)
 
 ## UTILISATION
 
-personne_1 = Personne("coucou")
-personne_2 = Personne("zob")
+personne_1 = Personne("coucou", 1)
+personne_2 = Personne("zob", 3)
 
 personne_1.Se_presenter()
 personne_2.Se_presenter()
+
+personne_2.nom = "Champion"
+
+personne_1.Donner_son_nom_et_son_age()
+personne_2.Donner_son_nom_et_son_age()
+
 
 
