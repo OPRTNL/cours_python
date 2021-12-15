20cd8672ece2ce53b44bc3ddba3674273c37c955
maxime berger <berger.pro.maxime@gmail.com>
2021-12-15 19:27:23 +0100

    is major ok

--- niveau_2/POO/main.py
+++ niveau_2/POO/main.py
@@ -1,6 +1,8 @@
 
 ## DEFINITION 
 
+
+
 class Personne:
     def __init__(self, nom : str, age : int):
         self.nom = nom
@@ -12,10 +14,14 @@ class Personne:
     def Se_presenter(self):
         print("Bonjour je m'appelle : " + self.nom)
 
+    def is_major(self):
+        return self.age >= 18
+
+
 ## UTILISATION
 
 personne_1 = Personne("coucou", 1)
-personne_2 = Personne("zob", 3)
+personne_2 = Personne("zob", 23)
 
 personne_1.Se_presenter()
 personne_2.Se_presenter()
@@ -25,5 +31,5 @@ personne_2.nom = "Champion"
 personne_1.Donner_son_nom_et_son_age()
 personne_2.Donner_son_nom_et_son_age()
 
-
-
+print(personne_1.is_major())
+print(personne_2.is_major())
