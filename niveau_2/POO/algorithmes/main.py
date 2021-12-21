8b62f5b700582461493005e48460e5b6d7c15237
maxime berger <berger.pro.maxime@gmail.com>
2021-12-21 11:57:05 +0100

    min et max done

--- niveau_2/POO/algorithmes/main.py
+++ niveau_2/POO/algorithmes/main.py
@@ -1,6 +1,19 @@
+# Renverser la valeur d'un chaine de ccaractère
+
 c = "Bonjour toto"
 
 def reverse_string(str):
     return str[::-1]
 
 print(reverse_string(c))
+
+# Trouver le min et le max d'une phrase
+
+s = "Un chasseur sachant chasser doit savoir chasser sans son chien"
+
+def min_max(s):
+    result = s.split(" ")
+    result.sort(key = lambda x : len(x))
+    return (result[0], result[-1])
+
+print(min_max(s))
