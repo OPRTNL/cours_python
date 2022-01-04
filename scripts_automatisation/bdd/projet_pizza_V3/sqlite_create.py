c2a8bf78f624a19d23be687b5ec92e72a65e5f71
OPRTNL <46159142+OPRTNL@users.noreply.github.com>
2022-01-04 18:10:19 +0100

    db pizza  creation and ingredient completion

--- scripts_automatisation/bdd/projet_pizza_V3/sqlite_create.py
+++ scripts_automatisation/bdd/projet_pizza_V3/sqlite_create.py
@@ -1,27 +1,39 @@
 import sqlite3
 
-connexion = sqlite3.connect("albums_1.db")
-curseur = connexion.cursor()
+connexion = sqlite3.connect("pizzas_1.db")
+c = connexion.cursor()
 
-curseur.execute("""CREATE TABLE artiste (
-    artiste_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-    nom VARCHAR);""")
+c.execute("""CREATE TABLE ingredient (
+    ingredient_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+    nom VARCHAR
+    );""")
 
-curseur.execute("""CREATE TABLE album (
-    album_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
-    artiste_id INTEGER REFERENCES artiste,
-    titre VARCHAR,
-    annee_sortie INTEGER);""")
+c.execute("""CREATE TABLE pizza (
+    pizza_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+    nom VARCHAR,
+    prix FLOAT,
+    ingredient_list_id REFERENCES ingredient_list,
+    vegan BOOLEAN
+    );""")
 
+c.execute("""CREATE TABLE ingredient_list (
+    ingredient_list_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    pizza_id REFERENCES pizza,
+    ingredient_id REFERENCES ingredient
+    );"""
+)
 
-curseur.execute("""INSERT INTO artiste (nom) VALUES ("Michael Jackson");""")
-mj_id = curseur.lastrowid
-curseur.execute("""INSERT INTO artiste (nom) VALUES ("Celine Dion");""")
-cd_id = curseur.lastrowid
-curseur.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({mj_id}, "Thriller", 1983);')
-curseur.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({cd_id}, "2X", 1992);')
-curseur.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({cd_id}, "Let\'s talk about love", 1997);')
+ingredients = ["Tomates","Mozzarella","Basilic","Jambon", "Champignon","Bleu", "Saint-Nectaire", "Comté", "Merguez"]
 
+for ingredient in ingredients:
+    c.execute(f"""INSERT INTO ingredient (nom) VALUES ("{ingredient}");""")
+
+#mj_id = c.lastrowid
+#c.execute("""INSERT INTO artiste (nom) VALUES ("Celine Dion");""")
+#cd_id = c.lastrowid
+#c.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({mj_id}, "Thriller", 1983);')
+#c.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({cd_id}, "2X", 1992);')
+#c.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({cd_id}, "Let\'s talk about love", 1997);')
 
 connexion.commit()
 connexion.close()
