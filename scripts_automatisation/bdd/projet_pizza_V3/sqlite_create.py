a1abcc6561f2025a5e981b17ac65954aeb2961cd
OPRTNL <46159142+OPRTNL@users.noreply.github.com>
2022-01-05 17:33:38 +0100

    create ingredient list ok

--- scripts_automatisation/bdd/projet_pizza_V3/sqlite_create.py
+++ scripts_automatisation/bdd/projet_pizza_V3/sqlite_create.py
@@ -23,17 +23,34 @@ c.execute("""CREATE TABLE ingredient_list (
     );"""
 )
 
-ingredients = ["Tomates","Mozzarella","Basilic","Jambon", "Champignon","Bleu", "Saint-Nectaire", "Comté", "Merguez"]
+#ingredients = ["Tomates","Mozzarella","Basilic","Jambon", "Champignon","Bleu", "Saint-Nectaire", "Comté", "Merguez"]
 
-for ingredient in ingredients:
-    c.execute(f"""INSERT INTO ingredient (nom) VALUES ("{ingredient}");""")
+#for ingredient in ingredients:
+#    c.execute(f"""INSERT INTO ingredient (nom) VALUES ("{ingredient}");""")
 
-#mj_id = c.lastrowid
-#c.execute("""INSERT INTO artiste (nom) VALUES ("Celine Dion");""")
-#cd_id = c.lastrowid
-#c.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({mj_id}, "Thriller", 1983);')
-#c.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({cd_id}, "2X", 1992);')
-#c.execute(f'INSERT INTO album (artiste_id, titre, annee_sortie) VALUES ({cd_id}, "Let\'s talk about love", 1997);')
+
+
+c.execute('INSERT INTO pizza (nom, prix, vegan) VALUES ("Margarita", 8.9, True)')
+c.execute('INSERT INTO pizza (nom, prix, vegan) VALUES ("Reine", 7.6, False)')
+c.execute('INSERT INTO pizza (nom, prix, vegan) VALUES ("4 fromages", 10.3, True)')
+c.execute('INSERT INTO pizza (nom, prix, vegan) VALUES ("Orientale", 8.9, False)')
+
+pizza_ingredients = ([1, ["Tomates","Mozzarella","Basilic"]], [2,["Tomates","Mozzarella","Jambon", "Champignon"]], [3,["Tomates","Mozzarella","Bleu", "Saint-Nectaire", "Comté"]],[4,["Tomates","Mozzarella", "Merguez", "Basilic"]])
+
+def search_name_in_db(table : str, occurence : str):
+    c.execute(f'SELECT * FROM {table} where nom = "{occurence}"')
+    query = c.fetchall()
+    print(query)
+    if len(query) == 0: return False
+    return query
+
+for pizza in pizza_ingredients:
+    for i in pizza[1]:
+        if not search_name_in_db("ingredient", i):
+            c.execute(f'INSERT INTO ingredient (nom) VALUES ("{i}");')
+        ingredient_index = search_name_in_db("ingredient",i)
+        c.execute(f'INSERT INTO ingredient_list (pizza_id,ingredient_id) VALUES ({pizza[0]}, {ingredient_index[0][0]})')
+
 
 connexion.commit()
 connexion.close()
