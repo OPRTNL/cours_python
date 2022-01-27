7a9d9b50f4a01dd1533d1f528b9636c30eddf8a2
OPRTNL <46159142+OPRTNL@users.noreply.github.com>
2022-01-27 13:41:32 +0100

    Front Menu done Main init

--- django/projet/pizzamama/menu/views.py
+++ django/projet/pizzamama/menu/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("coucou")
+    '''
+    pizzas = Pizza.objects.all()
+    pizzas_name_price = [f"{pizza.nom} : {pizza.prix}€ " for pizza in pizzas]
+    pizzas_str = ", ".join(pizzas_name_price)
+    return HttpResponse("Les pizzas : " + pizzas_str)
+    '''
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request,'menu/index.html',{'pizzas':pizzas})
+
+def cool(request):
+    return HttpResponse("ouais")
--- django/projet/pizzamama/pizzamama/urls.py
+++ django/projet/pizzamama/pizzamama/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('menu/', include('menu.urls')),
     path('admin/', admin.site.urls),
 ]
